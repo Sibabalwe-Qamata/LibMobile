249ab06a57c688a33abaf4081a1aaf1f723b9b2e
Ayabonga <nompetsheniayabonga@gmail>
2018-06-21 12:28:18 +0200

    added more discription to the page

---
+++ js/visitor.js
@@ -0,0 +1,459 @@
+function LibraryLocations() {
+
+  var location = {
+    "Simon's Town, Western Cape": {
+      latitude: 18.43675782,
+      longitude: -34.19380909
+    },
+    "Ocean View, Western Cape": {
+      latitude: 18.35168538,
+      longitude: -34.14861945
+    },
+    "Kommetjie , Western Cape": {
+      latitude: 18.32418372,
+      longitude: -34.14107253
+    },
+    "Fish Hoek , Western Cape": {
+      latitude: 18.4262567,
+      longitude: -34.13654747
+    },
+    "Muizenberg , Western Cape": {
+      latitude: 18.47074576,
+      longitude: -34.1067818
+    },
+    "Hangberg , Western Cape": {
+      latitude: 18.34198515,
+      longitude: -34.05339814
+    },
+    "Masakhane , Western Cape": {
+      latitude:   18.66870343,
+      longitude: - 34.04708542
+    },
+    "Imizamo Yethu, Western Cape": {
+      latitude:     18.36017137,
+      longitude:  - 34.02982609
+    },
+    "Hout Bay, Western Cape": {
+      latitude:     18.35836097 ,
+      longitude: - 34.04399894
+    },
+    "Camps Bay, Western Cape": {
+      latitude:    18.37765926,
+      longitude: - 33.95378538
+    },
+    "Crossroads, Western Cape": {
+      latitude:      18.59694877,
+      longitude: - 33.99829735
+    },
+    "Wesfleur, Western Cape": {
+      latitude:      18.493912 ,
+      longitude: - 33.56476809
+    },
+    " Mamre, Western Cape": {
+      latitude:       18.4764602,
+      longitude:- 33.51336321
+    },
+    " Avondale, Western Cape": {
+      latitude: 18.48596708,
+      longitude:-33.57066369
+    },
+    " Koeberg, Western Cape": {
+      latitude: 18.44599113,
+      longitude:-33.69311487
+    },
+    " Moses Mabhida, Western Cape": {
+      latitude: 18.65105351	,
+      longitude: -34.01362094
+    },
+    " Mfuleni, Western Cape": {
+      latitude: 18.68194942	,
+      longitude: 	-34.00394126
+    },
+    " 	Brown's Farm, Western Cape": {
+      latitude: 18.58538859,
+      longitude: 	-34.00561138
+    },
+    " Phillipi East, Western Cape": {
+      latitude: 18.60783875,
+      longitude: -34.01015166
+    },
+    " Maitland, Western Cape": {
+      latitude: 18.48707473,
+      longitude: -33.9235559
+    },
+    "Gordon's Bay , Western Cape": {
+      latitude: 18.87018468,
+      longitude:	-34.15708177
+    },
+    "Suider Strand , Western Cape": {
+      latitude: 18.84138728,
+      longitude:-34.12796685
+    },
+    "Strand , Western Cape": {
+      latitude: 18.83477516,
+      longitude:-34.08915583
+    },
+    "Strand , Western Cape": {
+      latitude: 18.84900011,
+      longitude:-34.08400518
+    },
+    "Somerset West , Western Cape": {
+      latitude: 18.84900011,
+      longitude:-34.08400518
+    },
+    "Strandfontein, Western Cape": {
+      latitude: 18.55453144,
+      longitude:-34.07209846
+    },
+    "Rocklands, Western Cape": {
+      latitude: 18.61130956,
+      longitude:-34.06456709
+    },
+    "Tokai, Western Cape": {
+      latitude:18.43873418,
+      longitude:	-34.06320128
+    },
+    "Tafelsig, Western Cape": {
+      latitude:18.63699329,
+      longitude:	-34.06191143
+    },
+    "Pelican Park , Western Cape": {
+      latitude:18.52709733,
+      longitude:-34.06182357
+    },
+    "Macassar , Western Cape": {
+      latitude:18.76356549,
+      longitude:-34.06263711
+    },
+    "Retreat , Western Cape": {
+      latitude:18.47618764,
+      longitude:-34.06299394
+    },
+    "Retreat , Western Cape": {
+      latitude:18.60295414,
+      longitude:-34.05218937
+    },
+    "Mitchell's Plain , Western Cape": {
+      latitude:18.62346141,
+      longitude:-34.04867507
+    },
+    "Grassy Park	 , Western Cape": {
+      latitude:18.4956521,
+      longitude:	-34.04906935
+    },
+    "Kulani, Western Cape": {
+      latitude:18.66014392,
+      longitude:-34.04236738
+    },
+    "Meadowridge, Western Cape": {
+      latitude:18.45424306,
+      longitude:-34.03911947
+    },
+    "Lotus River, Western Cape": {
+      latitude:18.52014717,
+      longitude:-34.04131441
+    },
+    "Ottery , Western Cape": {
+      latitude:18.51646303,
+      longitude:-34.02842626
+    },
+    "Ottery , Western Cape": {
+      latitude:18.57977644,
+      longitude:-34.01827679
+    },
+    "Weltevreden , Western Cape": {
+      latitude:18.57977644,
+      longitude:-34.01827679
+    },
+    "Khayelitsha , Western Cape": {
+      latitude:18.6655944,
+      longitude:-34.02614266
+    },
+    "Lentegeur , Western Cape": {
+      latitude:18.60978347,
+      longitude:-34.03380225
+    },
+    "Plumstead , Western Cape": {
+      latitude:18.46782409,
+      longitude:-34.02099283
+    },
+    "Southfield , Western Cape": {
+      latitude:18.4807621,
+      longitude:-34.03023781
+    },
+    "Eersteriver , Western Cape": {
+      latitude:18.72465962,
+      longitude:-34.00877047
+    },
+    "Wynberg , Western Cape": {
+      latitude:18.46760428,
+      longitude:-34.00522959
+    },
+    "Hanover Park , Western Cape": {
+      latitude:18.52725235,
+      longitude:-33.99480901
+    },
+    "	Melton Rose , Western Cape": {
+      latitude:18.71693763,
+      longitude:-33.98775837
+    },
+    "Lansdowne, Western Cape": {
+      latitude:18.50520564,
+      longitude:-33.99176628
+    },
+    "Nyanga, Western Cape": {
+      latitude:18.58651135,
+      longitude:-33.99563434
+    },
+    "Delft South, Western Cape": {
+      latitude:18.63921345,
+      longitude:-33.98600372
+    },
+    "Claremont, Western Cape": {
+      latitude:18.46741317,
+      longitude:-33.98538553
+    },
+    "Manenberg, Western Cape": {
+      latitude:18.55555152	,
+      longitude:-33.99155846
+    },
+    "Rylands, Western Cape": {
+      latitude:18.53436934,
+      longitude:-33.97044301
+    },
+    "Delft, Western Cape": {
+      latitude:18.64288956,
+      longitude:-33.97357894
+    },
+    "Guguletu, Western Cape": {
+      latitude:18.56719756,
+      longitude:-33.97681587
+    },
+    "Athlone, Western Cape": {
+      latitude:18.50480187,
+      longitude:-33.96067227
+    },
+    "Heideveld, Western Cape": {
+      latitude:18.55357426,
+      longitude:-33.96490838
+    },
+    "Rondebosch, Western Cape": {
+      latitude:18.47187734,
+      longitude:-33.96137552
+    },
+    "Bridgetown, Western Cape": {
+      latitude:18.5275876,
+      longitude:-33.95436443
+    },
+    "Valhalla Park, Western Cape": {
+      latitude:18.56995176,
+      longitude:-33.95300125
+    },
+    "Huguenot Square, Western Cape": {
+      latitude:18.64659705,
+      longitude:-33.94630667
+    },
+    "Mowbray, Western Cape": {
+      latitude:18.47100678,
+      longitude:-33.94912127
+    },
+    "Bishop Lavis, Western Cape": {
+      latitude:18.58315417,
+      longitude:-33.94925165
+    },
+    "Bonteheuwel, Western Cape": {
+      latitude:18.55239964,
+      longitude:-33.95145231
+    },
+    "Langa, Western Cape": {
+      latitude:18.52712895,
+      longitude:-33.94438414
+    },
+    "Belhar, Western Cape": {
+      latitude:18.61465799,
+      longitude:-33.94421554
+    },
+    "Adriaanse, Western Cape": {
+      latitude:18.58475578,
+      longitude:-33.93756723
+    },
+    "Vredehoek, Western Cape": {
+      latitude:18.42714387,
+      longitude:-33.93750558
+    },
+    "Observatory, Western Cape": {
+      latitude:18.47047918,
+      longitude:-33.93793033
+    },
+    "Kloof Street, Western Cape": {
+      latitude:18.40663307,
+      longitude:-33.93497936
+    },
+    "Paulse, Western Cape": {
+      latitude:18.67172195,
+      longitude:-33.93157914
+    },
+    "Pinelands, Western Cape": {
+      latitude:18.51156236,
+      longitude:-33.93432219
+    },
+    "Kuilsriver, Western Cape": {
+      latitude:18.67822409,
+      longitude:-33.92708792
+    },
+    "Woodstock, Western Cape": {
+      latitude:18.45028807,
+      longitude:-33.92877612
+    },
+    "Woodstock, Western Cape": {
+      latitude:18.45028807,
+      longitude:-33.92877612
+    },
+    "Elsies River, Western Cape": {
+      latitude:18.57399974,
+      longitude:-33.92896065
+    },
+    "Ravensmead, Western Cape": {
+      latitude:18.60333965,
+      longitude:-33.92240664
+    },
+    "Bellville South, Western Cape": {
+      latitude:18.6426775,
+      longitude:-33.91524959
+    },
+    "Kensington, Western Cape": {
+      latitude:18.51163727,
+      longitude:-33.90947026
+    },
+    "Brooklyn, Western Cape": {
+      latitude: 18.48999439,
+      longitude: -33.90039143
+    },
+    "Bellville, Western Cape": {
+      latitude: 18.62440363	,
+      longitude: -33.90222577
+    },
+    "Goodwood, Western Cape": {
+      latitude: 18.55059631	,
+      longitude:	-33.91131233
+    },
+    "Parow, Western Cape": {
+      latitude: 18.58710964	,
+      longitude:-33.90325917
+    },
+    "Tygervalley, Western Cape": {
+      latitude: 18.63429994	,
+      longitude:-33.874461
+    },
+    "Edgemead, Western Cape": {
+      latitude: 18.54279627	,
+      longitude:-33.8804455
+    },
+    "Scottsdene, Western Cape": {
+      latitude: 18.72150099	,
+      longitude:-33.866623
+    },
+    "Milnerton, Western Cape": {
+      latitude: 18.49795861,
+      longitude:-33.86819226
+    },
+    "Eikendal, Western Cape": {
+      latitude: 18.72196108,
+      longitude:-33.8525197
+    },
+    "Bothasig, Western Cape": {
+      latitude:18.54377239,
+      longitude:-33.8625753
+    },
+    "Kraaifontein, Western Cape": {
+      latitude:18.70548133,
+      longitude:-33.84124917
+    },
+    "Durbanville, Western Cape": {
+      latitude:18.64487048,
+      longitude:-33.82975281
+    },
+    "Table View, Western Cape": {
+      latitude:18.48816625,
+      longitude:-33.82777057
+    },
+    "Bloubergstrand, Western Cape": {
+      latitude:18.46021659,
+      longitude:-33.80099663
+    },
+    "Sir Lowry's Pass, Western Cape": {
+      latitude:18.9078949,
+      longitude:-34.1195685
+    },
+    "Brakenfell, Western Cape": {
+      latitude:18.68824381,
+      longitude:-33.87664545
+    },
+    "Leonsdale, Western Cape": {
+      latitude:18.57569542,
+      longitude:-33.91943353
+    },
+    "Lwandle, Western Cape": {
+      latitude:18.86488685,
+      longitude:-34.11856797
+    },
+    "Sea Point, Western Cape": {
+      latitude:18.39776526,
+      longitude:-33.90757012
+    },
+    "Central, Western Cape": {
+      latitude:18.42367592,
+      longitude:-33.9252791
+    },
+    "Nazeema Isaacs, Western Cape": {
+      latitude:18.70452824,
+      longitude:-34.04741615
+    },
+    "Masiphumilele, Western Cape": {
+      latitude:18.37815372,
+      longitude:-34.1283051
+    },
+    "Fisantekraal, Western Cape": {
+      latitude:18.71741369,
+      longitude:-33.78239463
+    },
+    "Harare, Western Cape": {
+      latitude:18.67135539,
+      longitude:-34.05836239
+    },
+    "Harare, Western Cape": {
+      latitude:18.67135539,
+      longitude:-34.05836239
+    },
+
+
+  };
+
+// 18.54929121	-33.97580758	NULL	Mobile	NULL	NULL	NULL	cnr Aries St + Gonubie Rd	 Surrey
+// 18.60838195	-33.99891165	NULL	Mobile	NULL	NULL	NULL	cnr Disa Rd + Lanzerac Rd	 Philippi
+// 18.5972363	-34.01625731	NULL	Mobile	NULL	NULL	NULL	cnr Heinz Park Rd + Daisy Cres.	Heinz Park	 Philippi
+// 18.41447275	-33.91881156	NULL	Mobile	NULL	NULL	NULL	cnr Dawes St + Longmarket St	 Scotche's Kloof
+// 18.44174255	-33.93505618	NULL	Mobile	NULL	NULL	NULL	cnr Worcester Rd + Perth Rd	 Woodstock
+// 18.55796121	-33.97634304	NULL	Mobile	NULL	NULL	NULL	cnr Jordan St + Silverstream Rd	 Silverstream	 Manenberg
+// 18.67703031	-33.98318525	NULL	Mobile	NULL	NULL	NULL	cnr Hindle Rd + Rottendam Rd	 Blue Downs
+// 18.50042841	-33.97625481	NULL	Mobile	NULL	NULL	NULL	cnr First Ave + Second Ave	 Rondebosch East
+// 18.50397301	-33.97631698	NULL	Mobile	NULL	NULL	NULL	cnr crawford Rd + Hayward Rd	 Crawford
+// 18.51582038	-33.98266518	NULL	Mobile	NULL	NULL	NULL	cnr Penlyn Ave + Belgravia Rd	 Belthorn Estate
+// 18.48273611	-33.97815187	NULL	Mobile	NULL	NULL	NULL	cnr Keurboom Rd + Belvedere Rd	 Claremont
+// 18.49779928	-33.95448549	NULL	Mobile	NULL	NULL	NULL	cnr Marivy St + Lyndale Rd	 Sybrand park
+// 18.63057493	-34.01509525	NULL	Mobile	NULL	NULL	NULL	cnr Auber Ave + Kipling Ave	 Mandalay
+// 18.4976332	-33.99890912	NULL	Mobile	NULL	NULL	NULL	cnr Maybury Rd + Roath St	 Lansdowne
+// 18.53479391	-33.98447878	NULL	Mobile	NULL	NULL	NULL	cnr Jim Francis Rd + Andries Rd	 Newfields	 Hanover Park
+// 18.54628802	-33.96660491	NULL	Mobile	NULL	NULL	NULL	cnr Reef Ave + Heideveld Rd	 Heideveld
+// 18.54306665	-33.97966689	NULL	Mobile	NULL	NULL	NULL	cnr Sirus Rd + Ruimte Way	 Greenhaven
+// 18.55306222	-33.97378769	NULL	Mobile	NULL	NULL	NULL	cnr Venster St + Cathedral Rd	 Manenberg
+// 18.48792642	-33.93514459	NULL	Mobile	NULL	NULL	NULL	cnr Deville Sq + Haig Ave	 Maitland Garden Village
+// 18.55854132	-33.91833625	NULL	Mobile	NULL	NULL	NULL	Vereeniging Circle	 Ruyterwacht
+// 18.53862823	-33.92144557	NULL	Mobile	NULL	NULL	NULL	cnr Sipres Ave + Chestnut St	 Thornton
+// 18.47052509	-34.04128576	NULL	Mobile	NULL	NULL	NULL	cnr Chadwin Rd + Waterford Rd	 Elfindale
+// 18.50275452	-34.02172586	NULL	Mobile	NULL	NULL	NULL	cnr Columbine Ave + Elm St	 Ottery
+// 18.48260662	-34.07281129	NULL	Mobile	NULL	NULL	NULL	cnr Southern Hills Dr + Harley St	 Lavender Hill
+// 18.48672915	-34.05769835	NULL	Mobile	NULL	NULL	NULL	cnr Peter Charles St + Candross Grant	 Cafda Village	 Retreat
+// 18.49254029	-34.03226356	NULL	Mobile	NULL	NULL	NULL	cnr Walmer Rd + Parkwood Ave	 Parkwood
+// 18.46563971	-34.07815941	NULL	Mobile	NULL	NULL	NULL	Agate Close	 Sheridon Park
+// 18.43909761	-34.07533824	NULL	Mobile	NULL	NULL	NULL	cnr Lynx Way + Westlake Drive	 Westlake
